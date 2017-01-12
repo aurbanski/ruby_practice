b5c260096b0dd98d8b7c508854b8acd370f0babf
aurbanski <aurbanski27@gmail.com>
2017-01-12 09:44:34 -0700

    Set up mre actions

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -1,4 +1,35 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article #{@article.title} Updated!"
+
+    redirect_to article_path(@article)
+  end
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    flash.notice = "Article #{@article.title} Deleted!"
+
+    redirect_to articles_path
+  end
+  def create
+    @article = Article.new(article_params)
+
+    @article.save
+
+    flash.notice = "Article #{@article.title} Saved!"
+    redirect_to article_path(@article)
+  end
+  def new
+    @article = Article.new
+  end
   def show
     @article = Article.find(params[:id])
   end
