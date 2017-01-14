803c083559b95047e8fbc8f63355358212423a0b
aurbanski <aurbanski27@gmail.com>
2017-01-14 16:18:28 -0700

    Sorcery authentication complete

--- blogger/app/controllers/comments_controller.rb
+++ blogger/app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_filter :require_login, except: [:create]
   def create
     @comment = Comment.new(comment_params)
     @comment.article_id = params[:article_id]
@@ -6,8 +7,9 @@ class CommentsController < ApplicationController
     @comment.save
 
     redirect_to article_path(@comment.article)
-end
+  end
 
-def comment_params
-  params.require(:comment).permit(:author_name, :body)
+  def comment_params
+    params.require(:comment).permit(:author_name, :body)
+  end
 end
--- blogger/app/controllers/tags_controller.rb
+++ blogger/app/controllers/tags_controller.rb
@@ -1,4 +1,5 @@
 class TagsController < ApplicationController
+  before_filter :require_login, only: [:destroy]
   def destroy
     @tag = Tag.find(params[:id])
     @tag.destroy
